7f795b5f2fc748ab59a809fce93ae8bdeebbe63b
rushikesh <dhawalerushikesh2002@gmail.com>
2024-06-12 03:56:56 +0530

    adding new file diff

---
+++ lib/diff.cpp
@@ -0,0 +1,17 @@
+🦚Hare Krishna 🙇
+Krishna wants you in his personal service 😃
+so Please Do your seva it  takes only <10 min 
+ys Rushikesh 
+Udemy logo
+The Ultimate MySQL Bootcamp: Go from SQL Beginner to ExpertFundamentals of Operating Systems%-----------ABOUT ME-----------
+
+
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+   cout <<"hello world"<<endl;
+    return 0; // Return success code
+}
+
